fc53fefe93c4d55e4cd9db4eb6b44fd05b2b4ade
parkjaewook1 <wodnr5050@gmail.com>
2024-09-28 16:53:14 +0900

    프로젝트 컨테이너 및 header navigation, 포트폴리오 Section 기본 구조 작성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import "./css/style.css";
+import {Helmet} from "react-helmet";
 
 function App() {
 
 
   return (
       <div className="conatiner">
-        <div>
-
-        </div>
+          {/* Dynamically configure <head> elements using Helmet */}
+          <div>
+              <Helmet>
+                  <title>박재형 웹 포트폴리오</title>
+              </Helmet>
+          </div>
+          {/* Header - navigation */}
+          <header>
+          </header>
+          {/* Intro Section */}
+          <section>
+          </section>
+          {/* About Section */}
+          <section>
+          </section>
+          {/* Skill Section */}
+          <section>
+          </section>
+          {/* Project Section */}
+          <section>
+          </section>
+          {/* Repository Section */}
+          <section>
+          </section>
+          {/* Contact Section */}
+          <section>
+          </section>
       </div>
   );
 }
