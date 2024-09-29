92a49ec890c2cd4f47efb5ed3c9214a971bf0b0e
parkjaewook1 <wodnr5050@gmail.com>
2024-09-29 20:03:52 +0900

    Typed.js 적용 및 boxicons 동적 로드 추가 - 홈 섹션에 Typed.js를 사용해 동적인 타이핑 애니메이션 추가 - useEffect 내에서 boxicons CSS를 동적으로 로드하여 아이콘 지원 - useRef를 사용하여 span 요소를 Typed.js와 연결 - useEffect 정리 함수에서 Typed.js 인스턴스 정리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import "./css/style.css";
 import {Helmet} from "react-helmet";
-import {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBlog} from "@fortawesome/free-solid-svg-icons";
+import Typed from "typed.js";
 
 function App() {
+    useEffect(() => {
+        const link = document.createElement("link");
+        link.href = "https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css";
+        link.rel = "stylesheet";
+        document.head.appendChild(link);
+    }, []);
+
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [isSticky, setIsSticky] = useState(false);
     const [activeSection, setActiveSection] = useState("");
+    const homeRef = useRef(null);
+    const el = useRef(null);
 
+        useEffect(() => {
+            const typed = new Typed(el.current, {
+                strings: [
+                    "효율적인 문제 해결을 추구합니다.",
+                    "새로운 도전에 적극적입니다.",
+                    "소통과 팀워크를 중시합니다.",
+                ],
+                typeSpeed: 100,
+                backSpeed: 100,
+                backDelay: 1000,
+                loop: true,
+            });
+            return () => {
+                typed.destroy();
+            };
+        }, []);
   return (
       <div className="conatiner">
           {/* Dynamically configure <head> elements using Helmet */}
@@ -57,7 +85,48 @@ function App() {
               </nav>
           </header>
           {/* Intro Section */}
-          <section>
+          <section className="home" id="home" ref={homeRef}>
+              <div className="home-img">
+                  {/*<Image className="home-image" src={homePic} alt=""/>*/}
+              </div>
+              <div className="home-content">
+                  <h3>안녕하세요, 새내기 개발자</h3>
+                  <h1>
+                      박재형 <span>입니다.</span>
+                  </h1>
+                  <h3 className="typed-js">
+                      저는, <span ref={el}></span>
+                  </h3>
+                  <br/>
+                  <p>
+                      창의적인 문제 해결 과정 중
+                  </p>
+                  <p>무한히 형성되는 지식은 저에게 마르지 않는 영감을 주며,</p>
+                  <p>
+                      열정을 불태우는 연료로 작용합니다.{" "}
+                  </p>
+                  <br/>
+                  <p>
+                      열정과 창의성으로 팀과 프로젝트에
+                  </p>
+                  <p>
+                      새로운 시각과 실질적인 개선을 가져올 수 있도록 하겠습니다.
+                  </p>
+                  <br/>
+                  <br/>
+                  <p className="home-content-footer"></p>
+                  <div className="social-media">
+                      <a href="https://github.com/chaiheung">
+                          <i className="bx bxl-github"></i>
+                      </a>
+                      <a href="">
+                          <FontAwesomeIcon icon={faBlog} />
+                      </a>
+                  </div>
+                  <a href="" className="btn" download>
+                      이력서 다운로드
+                  </a>
+              </div>
           </section>
           {/* About Section */}
           <section>
